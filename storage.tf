5618e4f91c759ba8550a0cb3ef77ce354781cece
Lance Kuo <lancekuo@gmail.com>
2018-04-26 23:30:55 -0400

    persistent storage updates, both terraform and docker compose script.

--- storage.tf
+++ storage.tf
@@ -66,21 +66,20 @@ resource "null_resource" "storage_trigger" {
         metric_id = "${ibm_storage_file.metrics.id}"
         logs_id = "${ibm_storage_file.logs.id}"
         data_id = "${ibm_storage_file.data.id}"
-        manager_id = "${element(ibm_compute_vm_instance.manager.*.id,1)}"
+        data_id = "${ibm_storage_file.certs.id}"
+        manager_id = "${element(ibm_compute_vm_instance.manager.*.id,0)}"
     }
 
     provisioner "remote-exec" {
         inline = [
             "mount ${ibm_storage_file.metrics.mountpoint} /mnt",
-            "echo '====== Metric Storage =====';if [ -d /mnt/prometheus ];then echo \"=> The directory has existed.\";else sudo mkdir /mnt/prometheus;sudo chmod 777 /mnt/prometheus;echo \"prometheus directory created.\";fi",
-            "umount /mnt",
+            "echo '====== Metric Storage =====';sudo chmod 777 /mnt; fi umount /mnt",
             "mount ${ibm_storage_file.logs.mountpoint} /mnt",
-            "echo '======Logs Storage=====';if [ -d /mnt/elasticsearch ];then echo \"=> The directory has existed.\";else sudo mkdir /mnt/elasticsearch;sudo chmod 777 /mnt/elasticsearch;echo \"elasticsearch directory created.\";fi",
-            "umount /mnt",
+            "echo '======Logs Storage=====';sudo chmod 777 /mnt;fi umount /mnt",
             "mount ${ibm_storage_file.data.mountpoint} /mnt",
             "echo '====== Data Storage =====';if [ -d /mnt/app ];then echo \"=> The directory has existed.\";else sudo mkdir /mnt/app;sudo chmod 777 /mnt/app;echo \"app directory created.\";fi",
-            "echo '====== PSQLStorage =====';if [ -d /mnt/data/postgresql ];then echo \"=> The directory has existed.\";else sudo mkdir -p /mnt/data/postgresql;sudo chmod 777 /mnt/data/postgresql;echo \"PSQL directory created.\";fi",
-            "umount /mnt",
+            "echo '====== PSQL Storage =====';if [ -d /mnt/data/postgresql ];then echo \"=> The directory has existed.\";else sudo mkdir -p /mnt/data/postgresql;sudo chmod 777 /mnt/data/postgresql;echo \"PSQL directory created.\";fi",
+            "echo '====== Fabric Storage(TMP) =====';if [ -d /mnt/fabric ];then echo \"=> The directory has existed.\";else sudo mkdir /mnt/fabric;sudo chmod 777 /mnt/fabric;echo \"Fabric directory created.\";fi; umount /mnt",
         ]
         connection {
             bastion_host        = "${ibm_compute_vm_instance.bastion.ipv4_address}"
@@ -89,7 +88,7 @@ resource "null_resource" "storage_trigger" {
 
             type                = "ssh"
             user                = "root"
-            host                = "${ibm_compute_vm_instance.manager.1.ipv4_address_private}"
+            host                = "${ibm_compute_vm_instance.manager.0.ipv4_address_private}"
             private_key         = "${file("${path.root}${var.rsa_key_manager["private_key_path"]}")}"
         }
     }
